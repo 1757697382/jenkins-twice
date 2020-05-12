d5a051be7ff859566b970169bad52c52f14ac003
GaoChao <48650335+1757697382@users.noreply.github.com>
2020-05-12 19:05:23 +0800

    Update config_private.py

--- config_private/config_private.py
+++ config_private/config_private.py
@@ -2,7 +2,7 @@
 USER = "is_user"
 PASS = "Utss9526."
 URL = "192.168.100.13"
-JENKINS_URL = 'http://is-jenkins.com/'
+JENKINS_URL = 'http://192.168.100.13:8080/'
 USER_ID = 'gaochao'
 API_TOKEN = '123456'
 
